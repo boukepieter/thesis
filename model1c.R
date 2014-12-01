f6b98a45f500cd773e03af87681209e9e5e79d83
boukepieter <boukepieter@gmail.com>
2014-12-01 11:36:17 +0200

    A start with version 1c

---
+++ model1c.R
@@ -0,0 +1,188 @@
+# Author: B.P. Ottow
+
+################################# Model starts ############################################
+HydroPowerMonthly <- function(DEMfile = "input/DEM.tif", Pdir = "input/P", ETdir = "input/ET",
+                       channelLength = 500, minimumHead = 25, minimumDebiet = 0.1, minimumPotential = 1000000,
+                       plotToGE = TRUE, work_env = "", plotMethod = "raster") {
+# load in input and packages
+require(raster)
+require(rgdal)
+require(sp)
+require(RSAGA)
+
+if(work_env == ""){
+  work_env <- rsaga.env()
+}
+
+dir.create("step")
+dir.create("output")
+
+ETfiles <- list.files(ETdir, pattern=".tif", full.names=TRUE)
+Pfiles <- list.files(Pdir, pattern=".tif", full.names=TRUE)
+noSteps <- min(length(ETfiles), length(Pfiles))
+
+ET <- stack(ETfiles[1:noSteps])
+P <- stack(Pfiles[1:noSteps])
+DEM <- raster(DEMfile)
+
+# runoff calc
+print("calculating runoff...")
+Presample <- resample(P, ET, method='bilinear')
+runoff <- Presample - ET
+runoffRes <- resample(runoff, DEM, method='bilinear')
+for (i in 1:noSteps){
+  writeRaster(runoffRes[[i]], filename=sprintf("step/runoffRes%02d.tif",i), format="GTiff", overwrite=TRUE)  
+}
+
+# Fill DEM:
+print("filling DEM...")
+rsaga.geoprocessor(lib="io_gdal", module=0,param=list(GRIDS=paste(getwd(),"step/DEM.sgrd",sep="/"), 
+                                                      FILES=paste(getwd(), DEMfile,sep="/"), 
+                                                      TRANSFORM=TRUE, INTERPOL=1),
+                   env=work_env, show.output.on.console = FALSE, warn = FALSE)
+rsaga.geoprocessor(lib="ta_preprocessor", module=3, 
+                   param=list(DEM="step/DEM.sgrd", RESULT="step/filledDEM.sgrd", MINSLOPE=0.01),
+                   env=work_env, show.output.on.console = FALSE, warn = FALSE)
+filledDEM <- raster("step/filledDEM.sdat")
+
+# Flow accumulation
+print("calculating flow accumulation...")
+for (i in 1:noSteps) {
+  rsaga.geoprocessor(lib="io_gdal", module=0,param=list(GRIDS=paste(getwd(),sprintf("step/runoffRes%02d.tif",i),sep="/"), # moet zonder getwd kunnen
+                                                        FILES=paste(getwd(),sprintf("step/runoffRes%02d.tif",i),sep="/"), 
+                                                        TRANSFORM=TRUE, INTERPOL=1),
+                     env=work_env, show.output.on.console = FALSE, warn = FALSE)
+  file.copy(c("step/filledDEM.sgrd", "step/filledDEM.sdat", "step/filledDEM.prj", "step/filledDEM.mgrd"),
+            c("step/runoffResa.sgrd", "step/runoffResa.sdat", "step/runoffResa.prj", "step/runoffResa.mgrd"))
+  rsaga.geoprocessor(lib="grid_tools", module=0,
+                     param=list(INPUT=sprintf("step/runoffRes%02d.sgrd",i), KEEP_TYPE=TRUE,
+                                TARGET=1, SCALE_UP_METHOD=1, SCALE_DOWN_METHOD=1,
+                                GRID_GRID="step/runoffResa.sgrd"),
+                     env=work_env, show.output.on.console = FALSE, warn = FALSE)
+  rsaga.geoprocessor(lib="grid_analysis", module=18, 
+                     param=list(SURFACE="step/filledDEM.sgrd", INPUT="step/runoffResa.sgrd",
+                                FLUX=sprintf("step/cell_acc%02d.sgrd",i), OPERATION=0),
+                     env=work_env, show.output.on.console = FALSE, warn = FALSE)
+  print(sprintf("%d step(s) finished", i))
+  file.remove(c("step/runoffResa.sgrd", "step/runoffResa.sdat", "step/runoffResa.prj", "step/runoffResa.mgrd"))
+}
+
+cellAcc <- stack(sprintf("step/cell_acc%02d.sdat", 1:noSteps))
+debiet <- cellAcc / 1000 / 24 / 3600 * res(cellAcc)[1] ^ 2
+
+# head
+print("calculating head...")
+source("costrasters.R")
+head <- HeadOnRiver.large(filledDEM, max(debiet), minimumDebiet=minimumDebiet, 
+                    channelLength=channelLength)
+writeRaster(head, filename="step/head.tif", format="GTiff", overwrite=TRUE)
+
+# hydro potential
+print("calculating potential...")
+potential <- debiet * head * 9.81 * 1000 # Joule / second
+
+# filters
+for (i in 1:noSteps){
+  potential[[i]][debiet[[i]] < minimumDebiet] <- NA
+  potential[[i]][head < minimumHead] <- NA
+}
+
+highPotential <- potential
+for (i in 1:noSteps){
+  highPotential[[i]][potential[[i]] < minimumPotential] <- NA # highpotential spots  
+}
+noOfPoints <- ncell(highPotential) - summary(highPotential)[6,]
+print("Number of points:")
+print(noOfPoints)
+
+# output
+print("saving output...")
+data(SAGA_pal)
+setwd("output")
+for (i in 1:noSteps){
+  writeRaster(potential[[i]], filename=sprintf("potential%02d.tif", i), format="GTiff", overwrite=TRUE)
+  #writeRaster(highPotential[[i]], filename=sprintf("highPotential%02d.tif", i), format="GTiff", overwrite=TRUE)
+  writeRaster(debiet[[i]], filename=sprintf("../step/debiet%02d.tif", i), format="GTiff", overwrite=TRUE)
+#   if(noOfPoints[i] == 0) {
+#     warning(paste("No points found with a potential higher than", minimumPotential, "in step", i))
+#     next
+#   }
+}
+
+# plot
+source("../plotPotential.R")
+names(potential) <- sprintf("potentialRaster%02d", seq(1,12))
+if (plotMethod == "raster"){
+  PlotTimeRaster()
+} else if (plotMethod == "vector"){
+  PlotTimeVector()
+} else if (plotMethod == "both") {
+  PlotTimeRaster()
+  PlotTimeVector()
+}
+
+
+}
+
+library(topmodel)
+head <- raster("step/head.tif")
+filledDEM <- raster("step/filledDEM.sdat")
+cellAcc <- stack(sprintf("step/cell_acc%02d.sdat", 1:noSteps))
+debiet <- cellAcc / 1000 / 24 / 3600 * res(cellAcc)[1] ^ 2
+potential <- debiet * head * 9.81 * 1000 # Joule / second
+
+# define point
+test <- potential[[1]]
+test[test < 750000] <- NA
+point <- rasterToPoints(test, spatial=TRUE)
+coord <- point@coords
+coord <- c(rowFromY(test, coord[2]), colFromX(test, coord[1]))
+
+# catchment
+catch <- subcatch(as.matrix(filledDEM), coord)
+cat <- filledDEM
+cat[] <- catch
+
+# topographic index
+topi <- topidx(as.matrix(filledDEM), res(filledDEM)[1])
+atb <- filledDEM
+atb[] <- topi$atb
+atb <- mask(atb, cat, maskvalue=0)
+topix <- make.classes(atb[],10)
+area <- filledDEM
+area[] <- topi$area
+plot(area)
+
+# parameters & delay
+data(huagrahuma)
+attach(huagrahuma)
+parameters["qs0"] <- 2e-4
+parameters["lnTe"] <- log
+parameters["m"] <- 0.07
+parameters["dt"] <- 24 * 30
+parameters["Sr0"] <- 0.15
+parameters["k0"] <- 0.02
+parameters["td"] <- 0.1
+parameters["CD"] <- 6.5
+
+delay
+
+# rain
+Pres <- resample(P, filledDEM, method="bilinear")
+r <- zonal(Pres, cat, 'mean')
+rain <- as.vector(r[2,2:length(r[2,])])
+rain <- rain / 1000 * 30
+
+# ET0
+ETres <- resample(ET, filledDEM, method="bilinear")
+et <- zonal(ETres, cat, 'mean')
+et0 <- as.vector(et[2,2:length(et[2,])]) + 0.5
+et0 <- et0 / 1000 * 30
+
+save.image("/media/boukepieter/schijfje_ottow/thesis/workspace1b/topmodelEnvi.RData")
+# topmodel
+Qsim <- topmodel(parameters, topix, delay, rain, et0, verbose = TRUE)
+plot(as.vector(debiet[coord[1],coord[2]]), col="blue", type="l", ylim=c(0,4))
+points(Qsim$Q * topi$area[coord[1],coord[2]] / 30 / 24 / 3600, col="red", type="l")
+
+as.vector(debiet[coord[1],coord[2]])
---
+++ startupscript1c.R
@@ -0,0 +1,94 @@
+# Author: B.P. Ottow
+
+# parameters
+
+setwd("/media/boukepieter/schijfje_ottow/thesis/workspace1b")
+channelLength = 500
+minimumHead = 25
+minimumDebiet = 0.1
+minimumPotential = 100000
+
+# files
+ETdir <- "input/ET"
+Pdir <- "input/P"
+DEMfile <- "input/DEMs.tif"
+
+source("model1c.R")
+HydroPowerMonthly(DEMfile, Pdir, ETdir, minimumPotential = minimumPotential, minimumDebiet = minimumDebiet,
+           minimumHead = minimumHead, channelLength = channelLength)
+
+
+####################################-Preprosessing-##################################################
+library(raster)
+install.packages("gdalUtils")
+library(gdalUtils)
+
+utm51 = CRS('+proj=utm +zone=51 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
+wgs84 = CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+
+files <- list.files("Data/MOD16")
+dir.create("monthly")
+
+hdf4_dataset <- system.file(paste("Data/MOD16/", files[1], sep=""), package="gdalUtils")
+for (i in 1:length(files)){
+  gdal_translate(paste("Data/MOD16/", files[i], sep=""), 
+                 paste("Data/ET/", substring(files[i],1,42), "tif",sep=""), sd_index=1)
+  print(i)
+}
+
+files <- list.files("Data/ET")
+
+days <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+for (i in 1:12){
+  mod2010 <- raster(paste("Data/ET/", files[i], sep=""))
+  mod2011 <- raster(paste("Data/ET/", files[i+12], sep=""))
+  mod2012 <- raster(paste("Data/ET/", files[i+24], sep=""))
+  mod2013 <- raster(paste("Data/ET/", files[i+36], sep=""))
+  #mod2010s <- crop(mod2010, arealarge)
+  mod2010[mod2010>3000] <- NA
+  #mod2011s <- crop(mod2011, arealarge)
+  mod2011[mod2011>3000] <- NA
+  #mod2012s <- crop(mod2012, arealarge)
+  mod2012[mod2012>3000] <- NA
+  #mod2013s <- crop(mod2013, arealarge)
+  mod2013[mod2013>3000] <- NA
+  mod16 <- (mod2010 + mod2011 + mod2012 + mod2013) / 4 / days[i]
+  mod16utm = projectRaster(mod16, crs=utm51)
+  ET <- crop(mod16utm, projectarea)
+  writeRaster(ET, filename=paste("input/ET", i, ".tif", sep=""), format="GTiff", overwrite=TRUE)
+  print(i)
+}
+
+# TRMM
+setwd("/media/boukepieter/schijfje_ottow/thesis/data/TRMM/3-hourly-tiff")
+days <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+files <- list.files("monthly")
+for (i in 1:12){
+  setwd("/media/boukepieter/schijfje_ottow/thesis/data/TRMM/3-hourly-tiff")
+  trmm2010 <- raster(paste("monthly/", files[i], sep=""))
+  trmm2011 <- raster(paste("monthly/", files[i+12], sep=""))
+  trmm2012 <- raster(paste("monthly/", files[i+24], sep=""))
+  trmm2013 <- raster(paste("monthly/", files[i+36], sep=""))
+  trmm <- (trmm2010 + trmm2011 + trmm2012 + trmm2013) / 40 * 3 / days[i]
+  projection(trmm) <- wgs84
+  trmmutm = projectRaster(trmm, crs=utm51)
+  P <- crop(trmmutm, projectarea)
+  setwd("/media/boukepieter/schijfje_ottow/thesis/workspace1b")
+  writeRaster(P, filename=paste("input/P", i, ".tif", sep=""), format="GTiff", overwrite=TRUE)
+  print(i)
+}
+
+DEM <- raster("Data/DEM.tif")
+test <- raster("input/P1.tif")
+plot(test, main="August", col=rainbow(12))
+
+for (i in 2:12) {
+  m <- raster(paste("input/P", i, ".tif", sep=""))
+  test <- test + m
+}
+test <- test / 12
+plot(test)
+plot(P)
+
+
+
