78a3428c9a966a5afb6839d343836ff7a37b8a11
Bouke Pieter Ottow <boukepieter@gmail.com>
2015-04-02 16:26:26 +0200

    making graphs

--- lidar.R
+++ lidar.R
@@ -1,34 +1,32 @@
-setwd("E:/thesis/data/reference dem")
 library(sp)
 library(rgdal)
 library(plotKML)
 library(raster)
 library(gstat)
 
-utm10 <- CRS("+proj=utm +zone=10 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
-lasFiles <- list.files(getwd(), pattern=".laz")#, full.names=TRUE)
+setwd("E:/thesis/data/reference dem")
 
-file <- lasFiles[1]
+utm10 <- CRS("+proj=utm +zone=10 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+lasFiles <- list.files(paste(getwd(), "data", sep="/"), pattern=".laz")#, full.names=TRUE)
 process.laz <- function(file){
   system(sprintf('E:/thesis/LAStools/bin/lasground -i "%s" -olas', file))
-  system(sprintf('E:/thesis/LAStools/bin/las2txt -i "%s_1.las" -otxt -parse xyzc -sep comma', substr(file, 1, nchar(file)-4)))
+  system(sprintf('E:/thesis/LAStools/bin/las2txt -i "%s_1.las" -otxt -parse xyzc -sep comma', 
+                 substr(file, 1, nchar(file)-4)))
   res <- read.csv(sprintf("%s_1.txt", substr(file, 1, nchar(file)-4)), header=FALSE)
   res <- res[res[4]==2,]
   print(file)
   points <- res[sample(1:nrow(res),25),]
 }
-
 selected <- lapply(lasFiles, FUN=process.laz)
-test <- Reduce(function(...) merge(..., all=T), selected)
-points <- SpatialPointsDataFrame(test[,1:2], test, proj4string=utm10)
+merged <- Reduce(function(...) merge(..., all=T), selected)
+points <- SpatialPointsDataFrame(merged[,1:2], merged, proj4string=utm10)
 
-setwd("..")
 aster <- raster("ASTER/ASTGTM2_N45W122_dem.tif")
 asterUTM <- projectRaster(aster, crs=utm10)
 aster2 <- raster("ASTER/ASTGTM2_N46W122_dem.tif")
 asterUTM2 <- projectRaster(aster2, crs=utm10)
 
-plot(asterUTM)
+plot(asterUTM2)
 plot(points, add=T)
 extent(asterUTM)
 
@@ -37,65 +35,68 @@ points$asterValues <- asterValues
 points$difference <- points$V3 - points$asterValues
 
 save(points,file="points.Rda")
+load("points.Rda")
 writeRaster(asterUTM2, filename="asterUTM.tif" , format="GTiff", overwrite=TRUE)
 writeOGR(points, dsn=getwd(), layer= "points", 
          driver="ESRI Shapefile", overwrite=TRUE)
 
-## after reading
-points <- readOGR(getwd(), "points")
-points <- points[abs(points$diffrnc) < 1000,]
+## PLOT
+aster <- crop(asterUTM2, extent(points))
 
-points@data[abs(points$diffrnc) > 100,]
-hist(points$diffrnc, breaks=100, xlim=c(-100,100))
-mean(points$diffrnc)
-sd(points$diffrnc)
+plot(aster, main="ASTER GDEM and location and error of the lidar points")
+plot(points, add=T, pch=21, cex=0.5, col="black", bg="green")
+plot(errorpoints3, add=T, pch=21, cex=0.5, bg="yellow")
+plot(errorpoints2, add=T, pch=21, cex=0.5, bg="red")
+plot(errorpoints, add=T, pch=21, cex=0.5, bg='purple')
+legend( "bottomright"
+        #, inset = c(0,0.4), 
+        , cex = 1, 
+        #, bty = "n", 
+        , legend = c("> 1000 m", "> 50 m", "> 20 m", "< 20 m"), 
+        #, text.col = c("red", "blue"),
+        , col = rep("black", 4), 
+        , pt.bg = c("purple", "red","yellow", "green")
+        , pch = c(21,21,21)
+      , title= "DEM error in points"
+)
+
+errorpoints3 <- points[abs(points$difference) > 20,]
+
+points <- readOGR(getwd(), "points")
+points <- points[abs(points$difference) < 1000,]
+
+points@data[abs(points$difference) > 100,]
+
+m <- mean(points$difference)
+sd <- sd(points$difference)
+h <- hist(points$difference, breaks=100, xlim=c(-100,100), 
+     main="Histogram of points elevation - ASTER GDEM elevation",
+     xlab="reference points - ASTER GDEM", col="grey", border="grey")
+segments(m, 0, m, 150, lwd = 2, lty=2)
+segments(m-sd, 0, m-sd, 150, lwd = 2, lty=3)
+segments(m-2*sd, 0, m-2*sd, 150, lwd = 2, lty=3)
+segments(m+sd, 0, m+sd, 150, lwd = 2, lty=3)
+segments(m+2*sd, 0, m+2*sd, 150, lwd = 2, lty=3)
+
+xfit<-seq(min(points$difference),max(points$difference),length=400) 
+yfit<-dnorm(xfit,mean=m,sd=sd)
+yfit <- yfit*diff(h$mids[1:2])*length(points$difference) 
+lines(xfit, yfit, col="black", lwd=1)
 
 # gstat
-g <- gstat(formula = diffrnc~1, data = points)
-variogram <- variogram(g)
+g <- gstat(formula = difference~1, data = points)
 variogram <- variogram(g, boundaries = c(0, 1:10*50, seq(600,1200,by=100)))
-plot(variogram)
-
 vgm <- vgm(nugget = 0, psill = 300, range = 250, model = "Exp")
-plot(variogram, vgm, plot.nu = T)
 vgmf <- fit.variogram(variogram, vgm, fit.method=7)
-plot(variogram, vgmf, plot.nu = T)
+
+plot(variogram, vgmf, plot.nu = T, main="Semivariogram for the DEM error")
 vgmf
 
 # SSErr attribute is (weighted) sum of squared errors according to fit criterion
 attr(vgmf, "SSErr")
 
-# simulation of maps
-geul <- read.table("SMS/geuldata.txt", header = TRUE)
-coordinates(geul) <- ~x+y
-mask <- readGDAL("SMS/geul_mask.txt")
-gpb <- gstat(id = c("pb"), formula = pb~1, data = geul)
-vgpb <- variogram(gpb, boundaries = c(50,100,150,200,300,400,600,800,1000))
-vgmpb <- vgm(psill = 15000, range = 400, model = "Sph", add.to = vgm(psill = 1000, range = 100, model = "Sph"))
-vgmpb <- fit.variogram(vgpb,vgmpb)
-plot(vgpb,vgmpb, plot.numbers = TRUE)
 
-geul.krig <- krige(pb~1, geul, newdata = mask, vgmpb)
-MC <- 100
-set.seed(2373)
-geul.sim <- krige(pb~1, geul, newdata = mask, vgmpb, nsim = 100, nmax = 24)
-
-plot(asterUTM2)
-ext <- drawExtent()
-test <- crop(asterUTM2, ext)
-test <- as(test, 'SpatialGridDataFrame')
-test[!is.na(test@data)] <- 1
-projection(test) <- NA
-set.seed(2373)
-aster.sim <- krige(V3~1, points, newdata=test, model=vgmf, nsim = 3, nmax = 3)
-spplot (DEM.sim, zcol = c("sim1", "sim2", "sim3"), #xlim=c(190200,191300), ylim=c(314300,315600),
-        col.regions = bpy.colors())
-save(vgmf,file="vgmf.RData")
-
-spplot (DEM.sim[1], zcol = "sim1", #xlim=c(190200,191300), ylim=c(314300,315600),
-        col.regions = bpy.colors())
-
-#now for my study area:
+#simulation of maps:
 load("vgmf.RData")
 DEM <- raster("E:/thesis/workspace1b/input/DEM.tif")
 filledDEM <- raster("E:/thesis/workspace1b/step/filledDEM.tif")
